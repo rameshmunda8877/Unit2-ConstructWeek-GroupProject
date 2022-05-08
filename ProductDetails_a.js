db16437ffd235e124a03aca87b0b0955e5fb936d
KSrivastawa <kt.ketansri@gmail.com>
2022-05-08 21:04:33 +0530

    Product Category Page Added

---
+++ ProductDetails_a.js
@@ -0,0 +1,395 @@
+// objets details
+
+var productsDetails = [
+    {
+     offerPercentage:"Get 13% Off 🌟",
+     productImage:"https://www.bigbasket.com/media/uploads/p/s/1214632_2-eveready-carbon-zinc-battery-red-hd-aa-1015.jpg",
+     productName:"Eveready",
+     productDescription:"Carbon Zinc Battery Red HD AA 1015",
+     priceMode:"Rs",
+     productStrikeMRP:"300",
+     productMRP:"261",
+     productDelivery:"Standard Delivery Available",
+     cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+     priceFilter:"Rs 201 to Rs 500",
+     },
+
+     {
+        offerPercentage:"Get 22% Off 🌟",
+        productImage:"https://www.bigbasket.com/media/uploads/p/mm/40217157_1-panasonic-carbon-zinc-aaa-battery-gold-plus.jpg",
+        productName:"Panasonic",
+        productDescription:"Eneloop Rechargeable AAA Batteries - 800 MAH",
+        priceMode:"Rs",
+        productStrikeMRP:"800",
+        productMRP:"749",
+        productDelivery:"Standard Delivery Available",
+        cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+        priceFilter:"Rs 501 to Rs 1000",
+    },
+
+     {
+     offerPercentage:"Get 19% Off 🌟",
+     productImage:"https://www.bigbasket.com/media/uploads/p/s/240100_9-duracell-ultra-alkaline-battery-aa.jpg",
+     productName:"Duracell",
+     productDescription:"Ultra Alkaline Battery AA",
+     priceMode:"Rs",
+     productStrikeMRP:"170",
+     productMRP:"137",
+     productDelivery:"Standard Delivery Available",
+     cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+     priceFilter:"Rs 101 to Rs 200",
+     },
+
+     {
+     offerPercentage:"Get 20% Off 🌟",
+     productImage:"https://www.bigbasket.com/media/uploads/p/s/273623_5-duracell-ultra-alkaline-battery-aaa.jpg",
+     productName:"Duracell",
+     productDescription:"Ultra Alkaline Battery AAA",
+     priceMode:"Rs",
+     productStrikeMRP:"170",
+     productMRP:"136",
+     productDelivery:"Standard Delivery Available",
+     cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+     priceFilter:"Rs 101 to Rs 200",
+     },
+
+     {
+        offerPercentage:"Get 22% Off 🌟",
+        productImage:"https://www.bigbasket.com/media/uploads/p/mm/40217157_1-panasonic-carbon-zinc-aaa-battery-gold-plus.jpg",
+        productName:"Panasonic",
+        productDescription:"Eneloop Rechargeable AAA Batteries - 800 MAH",
+        priceMode:"Rs",
+        productStrikeMRP:"800",
+        productMRP:"749",
+        productDelivery:"Standard Delivery Available",
+        cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+        priceFilter:"Rs 501 to Rs 1000",
+    },
+    {
+        offerPercentage:"Get 46% Off 🌟",
+        productImage:"https://www.bigbasket.com/media/uploads/p/mm/40159815_2-agaro-led-bulb-7-watt-cool-daylight-b22-base.jpg",
+        productName:"Agaro",
+        productDescription:"LED Bulb - 7 Watt, Cool Daylight, B22 Base",
+        priceMode:"Rs",
+        productStrikeMRP:"140",
+        productMRP:"75",
+        productDelivery:"Standard Delivery Available",
+        cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+        priceFilter:"Rs 101 to Rs 200",
+        LED_Bulb_Wattage:"6 - 9 W",
+    },
+    {
+        offerPercentage:"Get 54% Off 🌟",
+        productImage:"https://www.bigbasket.com/media/uploads/p/mm/40128546_7-eveready-led-bulb-9-watt-cool-day-light-base-b22d.jpg",
+        productName:"Eveready",
+        productDescription:"LED Bulb - 9 Watt, Cool Day Light, Base B22D",
+        priceMode:"Rs",
+        productStrikeMRP:"150",
+        productMRP:"67",
+        productDelivery:"Standard Delivery Available",
+        cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+        priceFilter:"Rs 101 to Rs 200",
+        LED_Bulb_Wattage:"6 - 9 W",
+    },
+    {
+        offerPercentage:"Get 16% Off 🌟",
+        productImage:"https://www.bigbasket.com/media/uploads/p/mm/30007456_4-eveready-carbon-zinc-d-size-battery-heavy-duty-1050-r20.jpg",
+        productName:"Eveready",
+        productDescription:"Carbon Zinc D Size Battery - Heavy Duty, 1050 R20",
+        priceMode:"Rs",
+        productStrikeMRP:"70",
+        productMRP:"59",
+        productDelivery:"Standard Delivery Available",
+        cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+        priceFilter:"Rs 51 to Rs 100",
+    },
+
+        {
+        offerPercentage:"Get 47% Off 🌟",
+        productImage:"https://www.bigbasket.com/media/uploads/p/mm/1214631_1-energizer-alkaline-battery-power-aaa-long-lasting-power.jpg",
+        productName:"Energizer",
+        productDescription:"Alkaline Battery Power AAA - Long Lasting Power",
+        priceMode:"Rs",
+        productStrikeMRP:"180",
+        productMRP:"95",
+        productDelivery:"Standard Delivery Available",
+        cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+        priceFilter:"Rs 101 to Rs 200",
+        },
+        {
+            offerPercentage:"Get 42% Off 🌟",
+            productImage:"https://www.bigbasket.com/media/uploads/p/mm/1214618_1-agaro-led-bulb-9-watt-cool-daylight-b22-base.jpg",
+            productName:"Agaro",
+            productDescription:"LED Bulb - 9 Watt, Cool Daylight, B22 Base",
+            priceMode:"Rs",
+            productStrikeMRP:"300",
+            productMRP:"172",
+            productDelivery:"Standard Delivery Available",
+            cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+            priceFilter:"Rs 201 to Rs 500",
+            LED_Bulb_Wattage:"6 - 9 W",
+            },
+            {
+                offerPercentage:"Get 45% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/1214647_1-wipro-tejas-led-bulb-9-watt-white-cool-day-b22.jpg",
+                productName:"Wipro",
+                productDescription:"Tejas LED Bulb - 9 Watt, White, Cool Day, B22",
+                priceMode:"Rs",
+                productStrikeMRP:"350",
+                productMRP:"192",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 201 to Rs 500",
+                LED_Bulb_Wattage:"6 - 9 W",
+                },
+                {
+                    offerPercentage:"Get 29% Off 🌟",
+                    productImage:"https://www.bigbasket.com/media/uploads/p/mm/40181722_1-nippo-led-lamp-10-watt-cool-daylight-t-bulb-base-b22.jpg",
+                    productName:"Nippo",
+                    productDescription:"LED/T -Bulb - Cool Daylight White, 10 Watts, B22 Base",
+                    priceMode:"Rs",
+                    productStrikeMRP:"280",
+                    productMRP:"199",
+                    productDelivery:"Standard Delivery Available",
+                    cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                    priceFilter:"Rs 201 to Rs 500",
+                    LED_Bulb_Wattage:"10 - 14 W",
+                    },
+                    {
+                        offerPercentage:"Get 24% Off 🌟",
+                        productImage:"https://www.bigbasket.com/media/uploads/p/mm/1214638_2-nippo-battery-aaa-gold-15v-4dg.jpg",
+                        productName:"Nippo",
+                        productDescription:"Battery AAA Gold 15V 4DG",
+                        priceMode:"Rs",
+                        productStrikeMRP:"320",
+                        productMRP:"242",
+                        productDelivery:"Standard Delivery Available",
+                        cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                        priceFilter:"Rs 201 to Rs 500",
+                        },
+                        
+                {
+                offerPercentage:"Get 41% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/1214648_1-wipro-garnet-led-bulb-12-watt-white-cool-day-b22.jpg",
+                productName:"Wipro",
+                productDescription:"Garnet LED Bulb -12 Watt, White, Cool Day, B22",
+                priceMode:"Rs",
+                productStrikeMRP:"590",
+                productMRP:"347",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 501 to Rs 1000",
+                LED_Bulb_Wattage:"10 - 14 W",
+            },
+            {
+                offerPercentage:"Get 19% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40174570_2-hit-anti-mosquito-racquet-rechargeable-insect-killer-bat-with-led-light.jpg",
+                productName:"HIT",
+                productDescription:"Anti Mosquito Racquet - Rechargeable Insect Killer Bat With LED Light",
+                priceMode:"Rs",
+                productStrikeMRP:"565",
+                productMRP:"460",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 501 to Rs 1000",
+                LED_Bulb_Wattage:"3 - 5 W",
+            },
+            {
+                offerPercentage:"Get 15% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40158551_1-philips-led-bulb-4-watt-cool-daylight-ace-saver-base-b22.jpg",
+                productName:"Philips",
+                productDescription:"Ace Saver LED Bulb - Cool Daylight White, Round, 4 Watts, B22 Base",
+                priceMode:"Rs",
+                productStrikeMRP:"135",
+                productMRP:"115",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 101 to Rs 200",
+                LED_Bulb_Wattage:"3 - 5 W",
+            },
+            {
+                offerPercentage:"Get 63% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40189194_1-syska-syska-smart-led-bulb-9-watt-wi-fi-enabled-multicolour-ssk-smw-9w-1-pc.jpg",
+                productName:"Syska",
+                productDescription:"SSK-SMW 9W Smart LED Bulb with Wifi-Enabled - Multicolour, Round, 9 Watts, Retro Fit B22",
+                priceMode:"Rs",
+                productStrikeMRP:"1899",
+                productMRP:"699",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 1001 to Rs 2000",
+                LED_Bulb_Wattage:"6 - 9 W",
+            },
+            {
+                offerPercentage:"Save Rs 51 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40217155_1-panasonic-eneloop-rechargeable-aaa-batteries-800-mah.jpg",
+                productName:"Panasonic",
+                productDescription:"Eneloop Rechargeable AAA Batteries - 800 MAH",
+                priceMode:"Rs",
+                productStrikeMRP:"800",
+                productMRP:"749",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 501 to Rs 1000",
+            },
+            {
+                offerPercentage:"Save Rs 21 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40183484_3-panasonic-alkaline-battery-aaa-1b6.jpg",
+                productName:"Panasonic",
+                productDescription:"LR03TDG/1B6 Alkaline Battery - AAA, 1.5 V",
+                priceMode:"Rs",
+                productStrikeMRP:"240",
+                productMRP:"219",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 201 to Rs 500",
+            },
+            {
+                offerPercentage:"Save Rs 51 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40217156_1-panasonic-eneloop-pro-rechargeable-aa-batteries-2550-mah.jpg",
+                productName:"Panasonic",
+                productDescription:"Eneloop Pro Rechargeable AA Batteries - 2550 MAH",
+                priceMode:"Rs",
+                productStrikeMRP:"1200",
+                productMRP:"1149",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 1001 to Rs 2000",
+            },
+            {
+                offerPercentage:"Get 22% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40217157_1-panasonic-carbon-zinc-aaa-battery-gold-plus.jpg",
+                productName:"Panasonic",
+                productDescription:"Eneloop Rechargeable AAA Batteries - 800 MAH",
+                priceMode:"Rs",
+                productStrikeMRP:"800",
+                productMRP:"749",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 501 to Rs 1000",
+            },
+            {
+                offerPercentage:"Get 46% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40159815_2-agaro-led-bulb-7-watt-cool-daylight-b22-base.jpg",
+                productName:"Agaro",
+                productDescription:"LED Bulb - 7 Watt, Cool Daylight, B22 Base",
+                priceMode:"Rs",
+                productStrikeMRP:"140",
+                productMRP:"75",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 101 to Rs 200",
+                LED_Bulb_Wattage:"6 - 9 W",
+            },
+            {
+                offerPercentage:"Get 54% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40128546_7-eveready-led-bulb-9-watt-cool-day-light-base-b22d.jpg",
+                productName:"Eveready",
+                productDescription:"LED Bulb - 9 Watt, Cool Day Light, Base B22D",
+                priceMode:"Rs",
+                productStrikeMRP:"150",
+                productMRP:"67",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 101 to Rs 200",
+                LED_Bulb_Wattage:"6 - 9 W",
+            },
+            {
+                offerPercentage:"Get 16% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/30007456_4-eveready-carbon-zinc-d-size-battery-heavy-duty-1050-r20.jpg",
+                productName:"Eveready",
+                productDescription:"Carbon Zinc D Size Battery - Heavy Duty, 1050 R20",
+                priceMode:"Rs",
+                productStrikeMRP:"70",
+                productMRP:"59",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 51 to Rs 100",
+            },
+            {
+                offerPercentage:"Get 31% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40158558_1-philips-led-bulb-10-watt-cool-daylight-deco-ring.jpg",
+                productName:"Philips",
+                productDescription:"LED Bulb - 10 Watt, Cool Daylight, Deco Ring",
+                priceMode:"Rs",
+                productStrikeMRP:"350",
+                productMRP:"245",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 201 to Rs 500",
+                LED_Bulb_Wattage:"10 - 14 W",
+            },
+            {
+                offerPercentage:"Get 54% Off 🌟",
+                productImage:"https://www.bigbasket.com/media/uploads/p/mm/40125993_1-syska-led-bulb-15-watt-base-b22-ssk-srl-15w.jpg",
+                productName:"Syska",
+                productDescription:"Borosilicate Glass Food/Tiffin/Storage Container - With PP Lid, Square, Blue",
+                priceMode:"Rs",
+                productStrikeMRP:"499",
+                productMRP:"229",
+                productDelivery:"Standard Delivery Available",
+                cart:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GCBD--xef2TLpm3HV4oVp19NmtjESahEAg&usqp=CAU",
+                priceFilter:"Rs 201 to Rs 500",
+                LED_Bulb_Wattage:"15 - 20 W",
+            },
+           
+
+]
+
+
+    localStorage.setItem("productsDisplayed_LS", JSON.stringify(productsDetails));
+
+    var addCartProducts = [];
+
+    var miniBaskets=document.querySelector("#detailedProductViews");
+
+    function display(data){
+            miniBaskets.innerHTML="";
+        data.forEach(function(elem){
+
+            var productCard = document.createElement("div");
+
+            var offer = document.createElement("div");
+            var img = document.createElement("img");
+            var name = document.createElement("p");
+            var description = document.createElement("h4");
+            var pricemode = document.createElement("span");
+            var strike = document.createElement("s");
+            var MRP = document.createElement("span");
+            var delivery = document.createElement("p");
+            var cart = document.createElement("img");
+            var cart1 = document.createElement("button");
+            
+            
+            offer.textContent=elem.offerPercentage;
+            img.src=elem.productImage;
+            name.textContent=elem.productName;
+            description.textContent=elem.productDescription;
+            pricemode.textContent=elem.priceMode;
+            strike.textContent=elem.productStrikeMRP;
+            MRP.textContent=elem.productMRP;
+            delivery.textContent=elem.productDelivery;
+            cart.src=elem.cart;
+            cart1.innerText="Add to Cart"
+
+            cart1.addEventListener("click", function(){
+                AddToCart(elem)
+          })
+            
+            
+
+            productCard.append(offer,img,name,description,pricemode,strike,MRP,delivery,cart,cart1);
+
+            miniBaskets.append(productCard);
+
+        })
+        
+    }
+       display(productsDetails);
+
+       function AddToCart(elem){
+        addCartProducts.push(elem);
+   
+        localStorage.setItem("mini-cart", JSON.stringify(addCartProducts));
+        alert("Added succesfully")
+      }
