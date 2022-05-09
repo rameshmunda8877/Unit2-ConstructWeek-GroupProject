74c05020783d7594ddfe36d27c256da20e9042b0
khalidshah12 <kssshah6@gmail.com>
2022-05-09 11:01:16 +0530

    update by khalid for basket div of home page

--- home.js
+++ home.js
@@ -1,45 +1,52 @@
 
 // Read More
-function Moreless(){
-    var dot=document.getElementById("dot");
-    var individual=document.getElementById("para");
-    var butn=document.getElementById("btn1");
-    if (dot.style.display != "none"){
-     dot.style.display = "none";
-     individual.style.display="inline";
-     butn.innerHTML="Read Less";
-}
-else{
- dot.style.display = "inline";
-     individual.style.display="none";
-     butn.innerHTML="Read More...";
-}
+function Moreless() {
+  var dot = document.getElementById("dot");
+  var individual = document.getElementById("para");
+  var butn = document.getElementById("btn1");
+  if (dot.style.display != "none") {
+    dot.style.display = "none";
+    individual.style.display = "inline";
+    butn.innerHTML = "Read Less";
+  }
+  else {
+    dot.style.display = "inline";
+    individual.style.display = "none";
+    butn.innerHTML = "Read More...";
+  }
 
 }
 
 
-    const swiper = new Swiper('.swiper', {
-autoplay:{
-    display:1000,
-    disableOnIteraction:false,
-},
-   
-loop: true,
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+    display: 1000,
+    disableOnIteraction: false,
+  },
+
+  loop: true,
 
 
-pagination: {
-  el: '.swiper-pagination',
-  clickable:true,               
-},
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
-navigation: {
-  nextEl: '.swiper-button-next',
-  prevEl: '.swiper-button-prev',
-},
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
 
 });
 
+// chnges by Khlaid Shah Start
+var basketDiv = document.querySelector("#box2")
+basketDiv.addEventListener("click",goToBasketFunction)
+function goToBasketFunction(){
+  window.location.href = "./Cart/cart.html"
+}
+// chnges by Khlaid Shah End
 
 
 
