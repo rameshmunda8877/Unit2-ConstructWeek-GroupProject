99ae0ce989970c9d1a8162d989437ba3e183306e
Manoj Gajare <manojgajare5@gmail.com>
2022-05-09 12:12:21 +0530

    home page updated

--- home.js
+++ home.js
@@ -1,24 +1,19 @@
-const swiper = new Swiper('.swiper', {
-  autoplay: {
-    display: 1000,
-    disableOnIteraction: false,
-  },
-
-  loop: true,
-
-
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-
-});
+// Read More
+function Moreless() {
+  var dot = document.getElementById("dot");
+  var individual = document.getElementById("para");
+  var butn = document.getElementById("btn1");
+  if (dot.style.display != "none") {
+    dot.style.display = "none";
+    individual.style.display = "inline";
+    butn.innerHTML = "Read Less";
+  }
+  else {
+    dot.style.display = "inline";
+    individual.style.display = "none";
+    butn.innerHTML = "Read More...";
+  }
+}
 
 // chnges by Khlaid Shah Start
 var basketDiv = document.querySelector("#box2")
